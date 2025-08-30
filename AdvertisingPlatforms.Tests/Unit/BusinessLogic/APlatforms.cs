45ff7cf90ff0d0152a531ab64264290d6fce0e02
Andrew <andrew-1111111@yandex.ru>
2025-08-30 18:29:14 +0300

    Added new tests.

--- AdvertisingPlatforms.Tests/Unit/BusinessLogic/APlatforms.cs
+++ AdvertisingPlatforms.Tests/Unit/BusinessLogic/APlatforms.cs
@@ -1,14 +1,85 @@
 ﻿using AdvertisingPlatforms.BusinessLogic.APlatforms;
+using System.Text;
+using Xunit.Abstractions;
 
 namespace AdvertisingPlatforms.Tests.Unit.BusinessLogic
 {
-    public class APlatforms
+    public class APlatforms()
     {
-        private readonly ApPlatformsRepository _repository;
+        private readonly ApPlatformsRepository _repository = new();
 
-        public APlatforms()
+        [Fact]
+        public void SetPlatforms_WithValidData_Return_True()
         {
-            _repository = new ApPlatformsRepository();
+            var data = Encoding.UTF8.GetBytes("Яндекс.Директ:/ru");
+            var result = _repository.SetPlatforms(data, Encoding.UTF8);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void SetPlatforms_WithValidData_UpdatesCount()
+        {
+            var data = Encoding.UTF8.GetBytes("Яндекс.Директ:/ru");
+            _repository.SetPlatforms(data, Encoding.UTF8);
+
+            Assert.Equal(1, _repository.Count);
+            Assert.False(_repository.IsEmpty);
+        }
+
+        [Fact]
+        public void GetPlatforms_WithExistingLocation_Return_Platforms()
+        {
+            var data = Encoding.UTF8.GetBytes("Яндекс.Директ:/ru");
+            _repository.SetPlatforms(data, Encoding.UTF8);
+
+            var platforms = _repository.GetPlatforms("/ru");
+
+            Assert.Contains("Яндекс.Директ", platforms);
+        }
+
+        [Fact]
+        public void GetPlatforms_WithNonExistingLocation_Return_Empty()
+        {
+            var data = Encoding.UTF8.GetBytes("Яндекс.Директ:/ru");
+            _repository.SetPlatforms(data, Encoding.UTF8);
+
+            var platforms = _repository.GetPlatforms("/nonexistent");
+
+            Assert.Empty(platforms);
+        }
+
+        [Fact]
+        public void GetAllPlatforms_WithMultipleEntries_Return_AllData()
+        {
+            var testData = @"Яндекс.Директ:/ru
+Ревдинский рабочий:/ru/svrd/revda,/ru/svrd/pervik
+Газета уральских москвичей:/ru/msk,/ru/permobl,/ru/chelobl";
+
+            var data = Encoding.UTF8.GetBytes(testData);
+            if (_repository.SetPlatforms(data, Encoding.UTF8))
+            {
+                var allPlatforms = _repository.GetAllPlatforms();
+
+                Assert.Equal(6, _repository.Count);
+                Assert.Contains("/ru:Яндекс.Директ", allPlatforms);
+                Assert.Contains("/ru/svrd/revda:Яндекс.Директ,Ревдинский рабочий", allPlatforms);
+            }
+        }
+
+        [Fact]
+        public void SetPlatforms_WithMultipleLocationsPerPlatform_SplitsCorrectly()
+        {
+            var testData = "Ревдинский рабочий:/ru/svrd/revda,/ru/svrd/pervik";
+            var data = Encoding.UTF8.GetBytes(testData);
+            _repository.SetPlatforms(data, Encoding.UTF8);
+
+            var platforms1 = _repository.GetPlatforms("/ru/svrd/revda");
+            var platforms2 = _repository.GetPlatforms("/ru/svrd/pervik");
+
+            Assert.Contains("Ревдинский рабочий", platforms1);
+            Assert.Contains("Ревдинский рабочий", platforms2);
+            Assert.Equal(2, _repository.Count);
         }
 
         [Fact]
